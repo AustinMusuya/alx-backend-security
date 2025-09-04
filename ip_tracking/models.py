87219ba18415e9f9f914c877fbddf25b3e4e52bb
AustinMusuya <amusuya57@gmail.com>
2025-09-04 15:57:48 +0300

    feat(ip_tracking): add IP blacklisting with middleware and block_ip command

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -16,3 +16,13 @@ class RequestLog(models.Model):
 
     def __str__(self):
         return f"{self.ip_address} - {self.path} @ {self.timestamp}"
+
+
+class BlockedIP(models.Model):
+    """
+    Stores IP addresses that are blocked from accessing the application.
+    """
+    ip_address = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return self.ip_address
